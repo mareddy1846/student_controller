29c9d69abc2cd59f73f5ca711cfe85b58532f848
mareddy1846 <karthikm1846@gmail.com>
2023-04-17 15:38:36 -0400

    Buliding Rest API Student Controller

---
+++ src/main/java/net/java/springbootrestapiexample/SpringbootRestApiExampleApplication.java
@@ -0,0 +1,14 @@
+package net.java.springbootrestapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApiExampleApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootRestApiExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/java/springbootrestapiexample/SpringbootRestApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.java.springbootrestapiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
